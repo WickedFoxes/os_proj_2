32b63535fc388b089ddfa9d83ec5541c6fb7864d
sostar <sostar0832@cau.ac.kr>
2024-06-05 12:52:29 +0900

    vehicle

--- pintos_qemu/projects/crossroads/vehicle.c
+++ pintos_qemu/projects/crossroads/vehicle.c
@@ -92,7 +92,7 @@ static int try_move(int start, int dest, int step, struct vehicle_info *vi)
 		}
 	}
 
-	if(deadlock_zone_cnt >= 7 && !cur_deadlock_zone_check && next_deadlock_zone_check) return 1;	
+	// if(deadlock_zone_cnt >= 7 && !cur_deadlock_zone_check && next_deadlock_zone_check) return 1;	
 	/* lock next position */
 	lock_acquire(&vi->map_locks[pos_next.row][pos_next.col]);
 	if (vi->state == VEHICLE_STATUS_READY) {
