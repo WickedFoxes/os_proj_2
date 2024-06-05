602c2d78816f6ef1810a5ac0f43b9af1ab5987bc
sostar <sostar0832@cau.ac.kr>
2024-06-05 17:04:38 +0900

    vehicle

--- pintos_qemu/projects/crossroads/vehicle.c
+++ pintos_qemu/projects/crossroads/vehicle.c
@@ -86,7 +86,7 @@ static int try_move(int start, int dest, int step, struct vehicle_info *vi)
 	}
 
 	/* lock next position */
-	lock_acquire(&vi->map_locks[pos_next.row][pos_next.col]);
+	// lock_acquire(&vi->map_locks[pos_next.row][pos_next.col]);
 	if (vi->state == VEHICLE_STATUS_READY) {
 		/* start this vehicle */
 		vi->state = VEHICLE_STATUS_RUNNING;
